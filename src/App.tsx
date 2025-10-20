df3e656dd0a7262da8ba70ce2bb22864f9b4bb1f
Rafael <rafaelalvesmdo_oficial@hotmail.com>
2025-10-20 15:56:48 -0300

    First Commit

---
+++ src/App.tsx
@@ -0,0 +1,41 @@
+import { useRef } from "react";
+import Navbar from "./components/Navbar";
+import Home from "./sections/Home";
+import About from "./sections/About";
+import Gallery from "./sections/Gallery";
+import Video from "./sections/Video";
+import Footer from "./components/Footer";
+
+function App() {
+  // Sections References
+  const homeRef = useRef<HTMLElement>(null);
+  const aboutRef = useRef<HTMLElement>(null);
+  const galleryRef = useRef<HTMLElement>(null);
+  const videoRef = useRef<HTMLElement>(null);
+
+  return (
+    <div>
+      <Navbar
+        scrollToHome={() =>
+          homeRef.current?.scrollIntoView({ behavior: "smooth", block: "start" })
+        }
+        scrollToAbout={() =>
+          aboutRef.current?.scrollIntoView({ behavior: "smooth" })
+        }
+        scrollToGallery={() =>
+          galleryRef.current?.scrollIntoView({ behavior: "smooth" })
+        }
+        scrollToVideo={() =>
+          videoRef.current?.scrollIntoView({ behavior: "smooth" })
+        }
+      />
+      <Home ref={homeRef} />
+      <About ref={aboutRef} />
+      <Gallery ref={galleryRef} />
+      <Video ref={videoRef} />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
