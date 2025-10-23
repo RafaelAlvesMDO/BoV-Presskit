6511a7b49d2a323e2a1a05ef58dd75c9a4ec6be9
Rafael <rafaelalvesmdo_oficial@hotmail.com>
2025-10-23 18:22:21 -0300

    Feat: Added new sections and references.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useRef } from "react";
 import Navbar from "./components/Navbar";
 import Home from "./sections/Home";
+import Characters from "./sections/Characters";
 import About from "./sections/About";
 import Gallery from "./sections/Gallery";
 import Video from "./sections/Video";
@@ -10,6 +11,7 @@ function App() {
   // Sections References
   const homeRef = useRef<HTMLElement>(null);
   const aboutRef = useRef<HTMLElement>(null);
+  const charactersRef = useRef<HTMLElement>(null);
   const galleryRef = useRef<HTMLElement>(null);
   const videoRef = useRef<HTMLElement>(null);
 
@@ -17,13 +19,13 @@ function App() {
     <div className="font-medieval">
       <Navbar
         scrollToHome={() =>
-          homeRef.current?.scrollIntoView({
-            behavior: "smooth",
-            block: "start",
-          })
+          homeRef.current?.scrollIntoView({ behavior: "smooth" })
         }
         scrollToAbout={() =>
           aboutRef.current?.scrollIntoView({ behavior: "smooth" })
+        }
+        scrollToCharacters={() =>
+          charactersRef.current?.scrollIntoView({ behavior: "smooth" })
         }
         scrollToGallery={() =>
           galleryRef.current?.scrollIntoView({ behavior: "smooth" })
@@ -34,6 +36,7 @@ function App() {
       />
       <Home ref={homeRef} />
       <About ref={aboutRef} />
+      <Characters ref={charactersRef} />
       <Gallery ref={galleryRef} />
       <Video ref={videoRef} />
       <Footer />
